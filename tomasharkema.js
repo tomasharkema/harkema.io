6467140919f06b515e53a1ad484c8c13a736be46
Tomas Harkema <info@tomasharkema.nl>
2014-10-07 21:27:08 +0200

    eerste opzet

--- tomasharkema.js
+++ tomasharkema.js
@@ -1,7 +1,7 @@
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault("counter", 0);
-
+  /*
   Template.hello.helpers({
     counter: function () {
       return Session.get("counter");
@@ -14,10 +14,11 @@ if (Meteor.isClient) {
       Session.set("counter", Session.get("counter") + 1);
     }
   });
+  */
 }
 
 if (Meteor.isServer) {
-  Meteor.startup(function () {
+  Meteor.startup(function() {
     // code to run on server at startup
   });
 }
